0d909880b5e94290ef24dd62d2a5e6a3d35fe379
Bigyan-009 <72550635+Bigyan-009@users.noreply.github.com>
2022-02-05 20:12:34 +0545

    Update data.js

--- js/data.js
+++ js/data.js
@@ -10,7 +10,7 @@ var data = {
     {name: 'City', value: 'Damak'}
   ],
   github: [
-    {name: 'GitHub', value: 'Bigyan-009'}
+    {name: 'GitHub', value: 'https://github.com/Bigyan-009'}
   ],
   projects: [],
   resume: 'Bigyan_Dahal.pdf',
