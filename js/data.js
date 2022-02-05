c88c6c5ea013fd260ce2806fe04cb04ba891ebba
Bigyan-009 <72550635+Bigyan-009@users.noreply.github.com>
2022-02-05 19:28:03 +0545

    Add files via upload

---
+++ js/data.js
@@ -0,0 +1,22 @@
+var data = {
+  name : [
+    {name: 'Name', value: 'Bigyan Dahal'}
+  ],
+  bio: [
+    {name: 'Name', value: 'Bigyan Dahal'},
+    {name: 'Email', value: 'bigyandahal123@gmail.com'},
+    {name: 'Phone', value: '+9779816956394'},
+    {name: 'Country', value: 'Nepal'},
+    {name: 'City', value: 'Damak'}
+  ],
+  github: [
+    {name: 'GitHub', value: 'Bigyan Dahal'}
+  ],
+  projects: [],
+  resume: 'Bigyan_Dahal.pdf',
+  socials: [
+    {name: 'LinkedIn', value: 'https://www.linkedin.com/in/bigyan-dahal-038a2a230/'},
+    {name: 'GitHub', value: 'https://github.com/Bigyan-009'},
+    {name: 'Facebook', value:'https://www.facebook.com/bigyan.dahal.399/'}
+  ]
+}
